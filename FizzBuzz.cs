9d12026942c4fec4fb53a51852d55c649c15d7ee
Anthony Kiefner <Akiefner@LFF.com>
2012-02-17 10:49:50 -0500

    Current iteration 18 minutes, No Mouse

---
+++ FizzBuzz.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzKata2
+{
+	public class FizzBuzz
+	{
+		public bool IsDivisbleByThree(int p)
+		{
+			return (p % 3) == 0;
+		}
+
+		public string GetTextForNumber(int p)
+		{
+			bool isDivisbleByThree = IsDivisbleByThree(p);
+			bool isDivisibleByFive = IsDivisbleByFive(p);
+
+			if (isDivisibleByFive && isDivisbleByThree)
+			{
+				return "FIZZBUZZ";
+			}
+			if(isDivisibleByFive)
+			{
+				return "BUZZ";
+			}
+			else if (isDivisbleByThree)
+			{
+				return "FIZZ";
+			}
+			return p.ToString();
+		}
+
+		public bool IsDivisbleByFive(int p)
+		{
+			return (p % 5) == 0;
+		}
+
+
+		public void Process(int p)
+		{
+			for (int i = 1; i <= p; i++)
+			{
+				Console.WriteLine(GetTextForNumber(i));
+			}
+		}
+	}
+}
---
+++ FizzBuzzTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FizzBuzzKata2
+{
+	[TestFixture]
+	public class FizzBuzzTest
+	{
+		[Test]
+		public void CanInstantiateTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+			Assert.That(fb, Is.Not.Null);
+		}
+		[Test]
+		public void IsThreeDivisibleByTHreeTEst()
+		{
+			FizzBuzz fb = new FizzBuzz();
+			bool isDivisibleByThree = fb.IsDivisbleByThree(3);
+			Assert.That(isDivisibleByThree, Is.True);
+		}
+		[Test]
+		public void ReturnFizzIfDIvisbleByTHreeTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+			string returnTextForNumber = fb.GetTextForNumber(6);
+			Assert.That(returnTextForNumber, Is.EqualTo("FIZZ"));
+		}
+		[Test]
+		public void IsFiveDivisibleByFIveTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+			bool isDivisiblByFive = fb.IsDivisbleByFive(5);
+			Assert.That(isDivisiblByFive, Is.True);
+		}
+		[Test]
+		public void ReturnBUZZIfDivisbileByFiveTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+			string returnTextForNumber = fb.GetTextForNumber(10);
+			Assert.That(returnTextForNumber, Is.EqualTo("BUZZ"));
+		}
+		[Test]
+		public void ReturnFIZZBUZZIdDivisbleByThreeANdFiveTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+			string returnTextForNumber = fb.GetTextForNumber(15);
+			Assert.That(returnTextForNumber, Is.EqualTo("FIZZBUZZ"));
+		}
+		[Test]
+		public void ReturnAllOtherValues()
+		{
+			FizzBuzz fb = new FizzBuzz();
+			string returnTextForNumber = fb.GetTextForNumber(4);
+			Assert.That(returnTextForNumber, Is.EqualTo("4"));
+		}
+		[Test]
+		public void ReturnAllValueTest()
+		{
+			FizzBuzz fb = new FizzBuzz();
+			fb.Process(200);
+		}
+
+	}
+}
